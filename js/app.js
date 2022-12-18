3b397b58d9981e0952081bb8c117d8ca23cd7b28
theomorphic <FelixMK@yandex.ru>
2022-12-19 02:02:04 +0500

    refactor: add new slider pics

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 $(function(){
 
-	$('.bike-slider, .slider__items').slick({
+	$('.bike-slider').slick({
 		arrows: false,
 		dots: true,
 		fade: true,
 		autoplay: true,
 		autoplaySpeed: 2500
 	});
+
+	$('.slider__items').slick({
+		arrows: false,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 4500
+	});
+
 
 	// $(".menu, .bike__column").on("click","a", function (event) {
 	// 	//отменяем стандартную обработку нажатия по ссылке
